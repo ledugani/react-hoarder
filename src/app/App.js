1cdce07913d82d16ca84c69111db70c93ac62b34
Tom Dugan <thomas@aadwebb.com>
2018-07-11 18:57:13 -0500

    displays each component

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import AllTheStuff from '../components/AllTheStuff/AllTheStuff';
+import Items from '../components/Items/Items';
+import Login from '../components/Login/Login';
+import MyStuff from '../components/MyStuff/MyStuff';
+import Navbar from '../components/Navbar/Navbar';
+import Register from '../components/Register/Register';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AllTheStuff />
+        <Items />
+        <Login />
+        <MyStuff />
+        <Navbar />
+        <Register />
       </div>
     );
   }
