f1827cacffad018988aab4e0d646f96922f666e5
Tom Dugan <thomas@aadwebb.com>
2018-07-11 19:32:36 -0500

    only shows register comp

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import {Route, BrowserRouter, Switch} from 'react-router-dom';
 
-import AllTheStuff from '../components/AllTheStuff/AllTheStuff';
-import Items from '../components/Items/Items';
-import Login from '../components/Login/Login';
-import MyStuff from '../components/MyStuff/MyStuff';
+// import AllTheStuff from '../components/AllTheStuff/AllTheStuff';
+// import Items from '../components/Items/Items';
+// import Login from '../components/Login/Login';
+// import MyStuff from '../components/MyStuff/MyStuff';
 import Navbar from '../components/Navbar/Navbar';
 import Register from '../components/Register/Register';
 
@@ -13,12 +14,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AllTheStuff />
-        <Items />
-        <Login />
-        <MyStuff />
-        <Navbar />
-        <Register />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <Route path="/" exact component={Register} />
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
