6441241bca94c15338e87d7e666494083ea5f690
Tom Dugan <thomas@aadwebb.com>
2018-07-14 12:36:54 -0500

    started adding items data

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -42,7 +42,7 @@ class Login extends React.Component {
     return (
       <div id="login-form">
         <h1 className="Login">Login</h1>
-        <form className="form-horizontal col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
+        <form className="form-horizontal col-sm-6 col-sm-offset-3">
           <div className="form-group">
             <label htmlFor="inputEmail" className="col-sm-2 control-label">
               Email:
@@ -59,10 +59,10 @@ class Login extends React.Component {
             </div>
           </div>
           <div className="form-group">
-            <label htmlFor="inputPassword" className="col-sm-2 control-label">
+            <label htmlFor="inputPassword" className="col-sm-4 control-label">
               Password:
             </label>
-            <div className="col-sm-10">
+            <div className="col-sm-8">
               <input
                 type="password"
                 className="form-control"
@@ -74,12 +74,12 @@ class Login extends React.Component {
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-offset-2 col-sm-10 text-center">
+            <div className="col-sm-12 text-center">
               <Link to="/register">Need to Register?</Link>
             </div>
           </div>
           <div className="form-group">
-            <div className="col-sm-offset-2 col-sm-10">
+            <div className="col-sm-12">
               <button
                 type="submit"
                 className="btn btn-default col-xs-12"
